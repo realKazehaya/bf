a27e4f61e103cf7e4a370e1c3d9f2bd8551d0987
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-01 18:50:29 -0400

    first c

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Activate the virtual environment
-source .venv/bin/activate
-
-# Run the Flask app using gunicorn
-exec gunicorn --bind 0.0.0.0:$PORT app:app
+# Ejecutar la aplicación Flask con gunicorn
+gunicorn -b 0.0.0.0:8000 app:app
