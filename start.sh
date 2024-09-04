0ce915d70bd7d7bc17dc4f46ae8d0e953a2b6c4a
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-03 21:23:02 -0400

    pyw

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Activar el entorno virtual, si estás usando uno
-# source venv/bin/activate
-
-# Ejecutar las migraciones de la base de datos (opcional, si estás usando algo como Alembic)
-# flask db upgrade
-
 # Iniciar la aplicación Flask
 python app.py &
 
-# Iniciar el bot de Discord
-python bot.py
+# Iniciar el bot de Discord desde la ruta correcta
+python discord_bot/bot.py
