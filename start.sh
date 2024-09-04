525ab405b6bca7e01bce4533f07f99537ba3e93f
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-03 21:18:46 -0400

    pyw

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-node app.js
+
+# Activar el entorno virtual, si estás usando uno
+# source venv/bin/activate
+
+# Ejecutar las migraciones de la base de datos (opcional, si estás usando algo como Alembic)
+# flask db upgrade
+
+# Iniciar la aplicación Flask
+python app.py &
+
+# Iniciar el bot de Discord
+python bot.py
