c5d0794f391b5f0afbef11ec2b8647e21594501c
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-04 23:22:00 -0400

    oq

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'your_secret_key')  # Ajustado el nombre de la variable de entorno
     SESSION_TYPE = 'filesystem'
-    
+
     DISCORD_CLIENT_ID = os.getenv('DISCORD_CLIENT_ID', 'your_client_id')
     DISCORD_CLIENT_SECRET = os.getenv('DISCORD_CLIENT_SECRET', 'your_client_secret')
-    DISCORD_REDIRECT_URI = os.getenv('DISCORD_REDIRECT_URI', 'http://localhost:5000/auth/callback')
+    DISCORD_REDIRECT_URI = os.getenv('DISCORD_REDIRECT_URI', 'http://localhost:5000/auth/callback')  # Asegúrate de agregarla en Render
     DISCORD_API_BASE_URL = 'https://discord.com/api'
 
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
