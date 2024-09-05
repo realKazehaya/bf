ed9c5fca1de2641970c5ed0960efd3b8090e0984
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-05 17:20:14 -0400

    auth21

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'your_secret_key')  # Ajustado el nombre de la variable de entorno
     SESSION_TYPE = 'filesystem'
 
-    DISCORD_CLIENT_ID = os.getenv('DISCORD_CLIENT_ID', 'your_client_id')
-    DISCORD_CLIENT_SECRET = os.getenv('DISCORD_CLIENT_SECRET', 'your_client_secret')
-    DISCORD_REDIRECT_URI = os.getenv('DISCORD_REDIRECT_URI', 'https://bf-t05l.onrender.com/auth/callback')  # Asegúrate de agregarla en Render
+    DISCORD_CLIENT_ID = os.getenv('DISCORD_CLIENT_ID', '1279932838631968851')
+    DISCORD_CLIENT_SECRET = os.getenv('DISCORD_CLIENT_SECRET', 'd5NsN3kx_--CKtN_JtRE7ITd8IaxhiYb')
+    DISCORD_REDIRECT_URI = os.getenv('DISCORD_REDIRECT_URI', 'https://bf-1.onrender.com/auth/callback')  # Asegúrate de agregarla en Render
     DISCORD_API_BASE_URL = 'https://discord.com/api'
 
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
