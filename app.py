b7fba25af71797eae9e5e87475ec58941f64b570
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-04 23:04:43 -0400

    rg

--- app.py
+++ app.py
@@ -31,6 +31,6 @@ def create_app():
 
     return app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=5000)
+f __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
