f74156ba0cce8cbcad05a23051cba6160cbde3f1
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-14 22:32:27 -0300

    yq

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,18 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres' });
+
+// Configuración de Sequelize con SSL
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Cambia esto a `true` en producción si usas un certificado válido
+    }
+  }
+});
 
 const User = sequelize.define('User', {
-  userId: {
+  freefire_id: { // Cambié el nombre del campo a freefire_id
     type: DataTypes.STRING,
     allowNull: false,
     unique: true
