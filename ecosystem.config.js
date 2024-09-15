1f544014fc2d1977799efa489e5d6123bf21486c
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-15 17:52:16 -0300

    ct

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,14 @@ module.exports = {
         name: 'app',
         script: './app.js',
         env: {
-          PORT: 3000
+          PORT: process.env.PORT || 3000
         }
       },
       {
         name: 'bot',
         script: './bot.js',
         env: {
-          PORT: 3000
+          PORT: process.env.PORT || 3000
         }
       }
     ]
