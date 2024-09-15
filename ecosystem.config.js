327e9583f86bfa0aa2d03ae5c29724666cc6ada4
leogsex <68473169+realKazehaya@users.noreply.github.com>
2024-09-15 15:24:17 -0300

    c

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
 module.exports = {
     apps: [
       {
-        name: 'app', // Nombre del proceso para tu aplicación principal
-        script: './app.js', // Ruta al archivo app.js
-        watch: true, // Reiniciar automáticamente si hay cambios en los archivos
+        name: 'app',
+        script: './app.js',
+        watch: true,
         env: {
-          NODE_ENV: 'production', // Modo de producción
-          PORT: 3000 // Puerto en el que se ejecutará app.js
+          NODE_ENV: 'production',
+          PORT: 3000
         }
       },
       {
-        name: 'bot', // Nombre del proceso para tu bot de Discord
-        script: './bot.js', // Ruta al archivo bot.js
-        watch: true, // Reiniciar automáticamente si hay cambios en los archivos
+        name: 'bot',
+        script: './bot.js',
+        watch: true,
         env: {
-          NODE_ENV: 'production', // Modo de producción
-          DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN // Token del bot de Discord desde las variables de entorno
+          NODE_ENV: 'production',
+          DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN
         }
       }
     ]
